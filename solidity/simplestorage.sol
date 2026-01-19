4d93b176743d8d3a5d3e09d65448aab4348540c6
levi1721rn <ipnitheshraj21@gmail.com>
2026-01-19 21:41:12 +0530

    wrote my first contract with store and retrieve functions

---
+++ solidity/simplestorage.sol
@@ -0,0 +1,30 @@
+//SPDX-LICENSE-Identifier: MIT
+pragma solidity ^0.8.18;  //stating our version 
+
+contract SimpleStorage{
+    /* 
+        
+         int256 is same as int
+        uint256 is same a unit 
+        bytes32 is the same as bytes
+        int256 variable_name;
+        this variable_name initializes to zero if no value is specified
+        i.e
+        int256 variable_name; is the same as
+        int256 variable_name = 0;
+
+
+    */
+     uint256 public favouriteNumber;  //0
+
+     function store(uint256 newFavouriteNumber) public {
+        favouriteNumber = newFavouriteNumber;
+     }
+     
+     // view and pure are functions we can call without having to send a transaction
+     //example for view function
+
+     function retreive() public view returns(uint256){
+        return favouriteNumber;
+     }
+}
